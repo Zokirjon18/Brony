8831c69f3b6764e96cd1e54db1adedf73c8c11bc
Zokirjon18 <tolqinovzokirjon347@gmail.com>
2025-07-10 10:51:02 +0500

    booking service added with some other code fixes

--- Brony/Extensions/Convertor.cs
+++ Brony/Extensions/Convertor.cs
@@ -1,4 +1,6 @@
-﻿namespace Brony.Extensions;
+﻿using Brony.Domain;
+
+namespace Brony.Extensions;
 
 public static class Convertor
 {
@@ -8,5 +10,31 @@ public static class Convertor
             .Select(i => text[i])
             .Select(c => (T)Convert.ChangeType(c, typeof(T)))
             .ToList();
+    }
+
+    public static List<Booking> ToBooking(this string text)
+    {
+        List<Booking> bookings = new List<Booking>();
+
+        string[] lines = text.Split('\n');
+
+        foreach (string line in lines)
+        {
+            if (string.IsNullOrWhiteSpace(line)) continue;
+
+            string[] parts = line.Split(',');
+
+            bookings.Add(new Booking
+            {
+                Id = int.Parse(parts[0]),
+                UserId = int.Parse(parts[1]),
+                StadiumId = int.Parse(parts[2]),
+                StartTime = DateTime.Parse(parts[3]),
+                EndTime = DateTime.Parse(parts[4]),
+                Price = decimal.Parse(parts[5])
+            });
+        }
+
+        return bookings;
     }
 }
--- Brony/Services/Bookings/IBookingService.cs
+++ Brony/Services/Bookings/IBookingService.cs
@@ -1,24 +1,21 @@
 using Brony.Domain;
+using Brony.Models;
 
 namespace Brony.Services.Bookings;
 
 public interface IBookingService
 {
-    void Book(
-        int userId, 
-        int stadiumId, 
-        DateTime startDate, 
-        DateTime endDate);
+    void Book(BookingCreateModel createModel);
 
     void Cancel(int bookingId);
 
     void ChangeDateTime(int bookingId, DateTime startDate, DateTime endDate);
 
     Booking Get(int id);
-    
+
     List<Booking> GetAll();
 
     List<Booking> GetAllByUserId(int userId);
-    
+
     List<Booking> GetAllByStadiumId(int stadiumId);
 }
