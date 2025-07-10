7c4765e480bc11820884517ee00687e2b96b19ae
Zokirjon18 <tolqinovzokirjon347@gmail.com>
2025-07-10 15:13:59 +0500

    stadium create model was in mess.i rewrote it and also created two models for create and update methods parameters

---
+++ Brony/Models/StadiumCreateModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brony.Models
+{
+    public class StadiumCreateModel
+    {
+        public string Name { get; set; }
+        public float Width { get; set; }
+        public float Length { get; set; }
+        public decimal Price { get; set; }
+        public string Location { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Description { get; set; }
+    }
+}
